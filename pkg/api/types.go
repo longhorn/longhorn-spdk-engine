c38fcad0bd56a26b5f6d7279797bcf64712d43a5
Derek Su <derek.su@suse.com>
2023-05-11 21:41:08 +0800

    Introduce block-type disk operations
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/api/types.go
+++ pkg/api/types.go
@@ -81,3 +81,16 @@ func ProtoEngineToEngine(e *spdkrpc.Engine) *Engine {
 
 	return res
 }
+
+type DiskInfo struct {
+	ID          string
+	UUID        string
+	Path        string
+	Type        string
+	TotalSize   int64
+	FreeSize    int64
+	TotalBlocks int64
+	FreeBlocks  int64
+	BlockSize   int64
+	ClusterSize int64
+}
--- pkg/spdk/types.go
+++ pkg/spdk/types.go
@@ -2,6 +2,11 @@ package spdk
 
 import "fmt"
 
+const (
+	DiskTypeFilesystem = "filesystem"
+	DiskTypeBlock      = "block"
+)
+
 func GetReplicaSnapshotLvolName(replicaName, snapshotName string) string {
 	return fmt.Sprintf("%s-snaps-%s", replicaName, snapshotName)
 }
