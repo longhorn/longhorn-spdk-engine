1e4978e9f8e2abed0cc053b5e671c12eaedb0112
Shuo Wu <shuo.wu@suse.com>
2023-06-08 22:23:30 +0800

    spdk: Fix Replica Snapshot APIs and snapshot-related issues
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- pkg/spdk/types.go
+++ pkg/spdk/types.go
@@ -1,14 +1,25 @@
 package spdk
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 const (
 	DiskTypeFilesystem = "filesystem"
 	DiskTypeBlock      = "block"
 )
 
+func GetReplicaSnapshotLvolNamePrefix(replicaName string) string {
+	return fmt.Sprintf("%s-snap-", replicaName)
+}
+
 func GetReplicaSnapshotLvolName(replicaName, snapshotName string) string {
-	return fmt.Sprintf("%s-snaps-%s", replicaName, snapshotName)
+	return fmt.Sprintf("%s%s", GetReplicaSnapshotLvolNamePrefix(replicaName), snapshotName)
+}
+
+func GetSnapshotNameFromReplicaSnapshotLvolName(replicaName, snapLvolName string) string {
+	return strings.TrimPrefix(snapLvolName, GetReplicaSnapshotLvolNamePrefix(replicaName))
 }
 
 func GetNvmfEndpoint(nqn, ip string, port int32) string {
