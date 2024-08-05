7ceda35b480aa1cc96a98549c17b5d74d060a83d
Chin-Ya Huang <chin-ya.huang@suse.com>
2024-08-05 10:28:26 +0800

    feat(restore): implement backup restore finish
    longhorn/longhorn-6613
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- pkg/spdk/engine.go
+++ pkg/spdk/engine.go
@@ -1652,6 +1652,14 @@ func (e *Engine) BackupRestore(spdkClient *spdkclient.Client, backupUrl, engineN
 		}
 	}
 
+	defer func() {
+		go func() {
+			if err := e.completeBackupRestore(spdkClient); err != nil {
+				logrus.WithError(err).Warn("Failed to complete backup restore")
+			}
+		}()
+	}()
+
 	resp := &spdkrpc.EngineBackupRestoreResponse{
 		Errors: map[string]string{},
 	}
@@ -1679,6 +1687,53 @@ func (e *Engine) BackupRestore(spdkClient *spdkclient.Client, backupUrl, engineN
 	}
 
 	return resp, nil
+}
+
+func (e *Engine) completeBackupRestore(spdkClient *spdkclient.Client) error {
+	if err := e.waitForRestoreComplete(); err != nil {
+		return errors.Wrapf(err, "failed to wait for restore complete")
+	}
+
+	return e.BackupRestoreFinish(spdkClient)
+}
+
+func (e *Engine) waitForRestoreComplete() error {
+	periodicChecker := time.NewTicker(time.Duration(restorePeriodicRefreshInterval.Seconds()) * time.Second)
+	defer periodicChecker.Stop()
+
+	for range periodicChecker.C {
+		isRestoring := false
+		for replicaName, replicaAddress := range e.ReplicaAddressMap {
+			log := e.log.WithFields(logrus.Fields{
+				"replica": replicaName,
+				"address": replicaAddress,
+			})
+			log.Trace("Checking replica restore status")
+
+			if isRestoring {
+				break
+			}
+
+			replicaServiceCli, err := GetServiceClient(replicaAddress)
+			if err != nil {
+				return errors.Wrapf(err, "failed to get replica %v service client %s", replicaName, replicaAddress)
+			}
+
+			status, err := replicaServiceCli.ReplicaRestoreStatus(replicaName)
+			if err != nil {
+				return errors.Wrapf(err, "failed to check replica %s restore status", replicaName)
+			}
+
+			isRestoring = status.IsRestoring
+		}
+
+		if !isRestoring {
+			e.log.Info("Backup restoration completed successfully")
+			return nil
+		}
+	}
+
+	return errors.Errorf("failed to wait for engine %s restore complete", e.Name)
 }
 
 func (e *Engine) BackupRestoreFinish(spdkClient *spdkclient.Client) error {
