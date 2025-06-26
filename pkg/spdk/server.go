81e1fc495c93f2d7e606f0cd8cf4e4b5320ae814
Shuo Wu <shuo.wu@suse.com>
2025-06-26 00:42:48 -0700

    fix: server should scan possible existing replicas when adding a new disk
    Longhorn 11188
    
    Signed-off-by: Shuo Wu <shuo.wu@suse.com>

--- pkg/spdk/server.go
+++ pkg/spdk/server.go
@@ -1617,7 +1617,33 @@ func (s *Server) DiskCreate(ctx context.Context, req *spdkrpc.DiskCreateRequest)
 	spdkClient := s.spdkClient
 	s.RUnlock()
 
-	return svcDiskCreate(spdkClient, req.DiskName, req.DiskUuid, req.DiskPath, req.DiskDriver, req.BlockSize)
+	ret, err = svcDiskCreate(spdkClient, req.DiskName, req.DiskUuid, req.DiskPath, req.DiskDriver, req.BlockSize)
+	if err != nil {
+		return nil, err
+	}
+
+	waitForScan := true
+	timer := time.NewTimer(3 * time.Minute)
+	defer timer.Stop()
+	ticker := time.NewTicker(MonitorInterval)
+	defer ticker.Stop()
+	for waitForScan {
+		select {
+		case <-s.ctx.Done():
+			logrus.Infof("SPDK gRPC server: cannot scan the newly added disk %s(%s) with path %s due to the context done", req.DiskName, req.DiskUuid, req.DiskPath)
+		case <-timer.C:
+			logrus.Infof("SPDK gRPC server: 3 minutes time out scanning the newly added disk %s(%s) with path %s, will continue", req.DiskName, req.DiskUuid, req.DiskPath)
+			waitForScan = false
+		case <-ticker.C:
+			err := s.verify()
+			if err == nil {
+				logrus.Infof("SPDK gRPC server: Scanned the newly added disk %s(%s) with path %s", req.DiskName, req.DiskUuid, req.DiskPath)
+				waitForScan = false
+			}
+		}
+	}
+
+	return ret, nil
 }
 
 func (s *Server) DiskDelete(ctx context.Context, req *spdkrpc.DiskDeleteRequest) (ret *emptypb.Empty, err error) {
