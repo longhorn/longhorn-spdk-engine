a9d187603a7f9ef7eb883359789f7211ee595521
bachmanity1 <bachmanity138@gmail.com>
2025-12-24 11:08:19 +0900

    fix: race condition in DiskGet
    Signed-off-by: bachmanity1 <bachmanity138@gmail.com>

--- pkg/spdk/disk/aio/aio.go
+++ pkg/spdk/disk/aio/aio.go
@@ -14,6 +14,7 @@ import (
 	spdkutil "github.com/longhorn/go-spdk-helper/pkg/util"
 
 	"github.com/longhorn/go-spdk-helper/pkg/jsonrpc"
+
 	"github.com/longhorn/longhorn-spdk-engine/pkg/spdk/disk"
 )
 
