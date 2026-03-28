1c2531ce005f3d21a276e1bb8db6b90b53a2b8da
Derek Su <derek.su@suse.com>
2026-03-28 14:58:13 +0000

    feat: move errors to pkg/spdk/types.go
    Longhorn 7124
    
    Signed-off-by: Derek Su <derek.su@suse.com>

--- pkg/spdk/recover_errors.go
+++
@@ -1,9 +0,0 @@
-package spdk
-
-import "github.com/cockroachdb/errors"
-
-var (
-	// ErrRecoverDeviceNotFound indicates the NVMe device was not found on the
-	// host during recovery. The persisted record should be removed.
-	ErrRecoverDeviceNotFound = errors.New("device not found on host during recovery")
-)
